aa0455415fabd883d13ed06d0fff9bc41c826a89
unicode <unicode@USPL-329>
2024-10-01 16:35:09 +0530

    Frist Commit java

---
+++ src/main/java/unierp/Openchrome.java
@@ -0,0 +1,414 @@
+package unierp;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Scanner;
+
+import org.openqa.selenium.WebDriver;
+
+/*public class Openchrome {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver =  new ChromeDriver();
+
+		driver.manage().window().maximize();
+		// Navigate to the desired URL
+		driver.get("https://dev-erpuni.mightcode.com/");
+		Thread.sleep(6000);
+		driver.quit();
+		
+	}
+
+}*/
+
+/*public class Openchrome{
+	public static void main(String[] argu) {
+		
+		int a=100, b=200;
+				int c;
+		c=a;
+		a=b;
+		b=c;
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+}*/
+
+
+/*public class Openchrome{
+	public static void main(String[] argu) {
+		int a=100, b=200, c;
+		c=a;
+		a=b;
+		b=c;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}*/
+
+/*public class Openchrome{
+	public static void main(String[] argu) {
+		int a=100, b=300, c;
+		c=a;
+		a=b;
+		b=c;
+		System.out.println("Now the value of a is "+ a);
+		System.out.println("Now the value of b is " + b);
+	}
+}*/
+
+/*public class Openchrome{
+	public static void main(String[] argu) {
+		int a=100, b=600, c;
+		c=a;
+		a=b;
+		b=c;
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+}*/
+/*public class Openchrome{
+	public static void main(String[] argu) {
+		int a=100, b=200;
+		System.out.println(a+b);
+	}
+}*/
+
+/*public class Openchrome{
+	public static void main(String[] argu) {
+		int a=100, b=200;
+		System.out.println(a<b);
+		System.out.println(a>b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+		System.out.println(a==b);
+		System.out.println(a!=b);
+	}
+}*/
+
+/*public class Openchrome{
+	public static void main(String[] argu) {
+		int age = 15;
+		if(age >= 18) {
+			System.out.println("Person can vote");
+		}
+		else {
+			System.out.print("Person can not vote");
+	}
+}
+}*/
+
+/*public class Openchrome{
+	public static void main(String[] argu) {
+		int age = 15;
+		if (age >= 18) {
+			System.out.println("eligible to vote");
+		}
+		else {
+			System.out.println("Not eligible to vote");
+		}
+	}
+}*/
+
+/*public class Openchrome{
+	public static void main(String[] argu) {
+		int num = 6;
+		if (num%2==0) {
+			System.out.println("number is even");
+		}
+		else {
+			System.out.println("Number is odd");
+		}
+	}
+}*/
+
+/*public class Openchrome{
+	public static void main(String[] argu) {
+		int num = 21;
+		if(num % 2 == 0) {
+			System.out.println("Number is even");
+		}
+		else {
+			System.out.println("Number is odd");
+		}
+	}
+}*/
+
+/*public class Openchrome{
+	public static void main(String[] argu){
+		int num = -1;
+		if (num > 0) {
+			System.out.println("Number is positive");
+		}
+		else 
+		{
+			System.out.println("Number is negative");
+		}
+	}
+}*/
+
+/*public class Openchrome{
+	public static void main(String[] argu) {
+		int num = -1;
+		if (num > 0) {
+			System.out.println("number is positive");
+		}
+		
+		else if (num < 0) {
+			System.out.println("number is negative");
+		}
+		
+		else {
+			System.out.print("zero");
+		}
+	}
+}
+*/
+
+/*public class Openchrome{
+	public static void main (String[] argu) {
+		int a=10, b=20, c=30;
+		if(a>b && a>c) {
+			System.out.println("a is the greatest");
+		}
+		else if(b>a && b>c) {
+			System.out.println("b is the greatest");
+		}
+		else if(c>a && c>b) {
+			System.out.println("c is the greatest number");
+		}
+	}
+}*/
+/*
+public class Openchrome{
+	public static void main(String[] argu) {
+		int num = 6;
+		switch(num) {
+		case 1: System.out.println("Sunday");
+		break;
+		case 2: System.out.println("Monday");
+		break;
+		case 3: System.out.println("Tuesday");
+		break;
+		case 4: System.out.println("Wednesday");
+		break;
+		case 5: System.out.println("Thursday");
+		break;
+		case 6: System.out.println("Friday");
+		break;
+		case 7: System.out.println("saturday");
+		break;
+		}
+	}
+}
+*/
+
+// WAP to check whether the year is leap or not 
+
+/*public class Openchrome{
+	public static void main(String[] argu) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int year = sc.nextInt();
+		
+		if(year % 4 == 0 ) {
+			System.out.print("The given year is leap year");
+		}
+			else if (year % 100 == 0) {
+				System.out.print("The given year is not leap year");
+			}
+			else if (year % 400 == 0) {
+				System.out.print("The given year is leap year");
+			}
+			else {
+				System.out.print("The given year is not a leap year");
+		
+	}
+}
+}
+*/
+// WAP to take a input from user in string
+//	public static void main(String[] argu) {
+//		Scanner sc= new Scanner(System.in);
+//		System.out.println("enter the text");
+//		String a = sc.nextLine();
+//		System.out.print(a);
+//	}
+//}
+
+
+// WAP to print the number from 1 to 10 using while loop
+//public class Openchrome{
+//	public static void main(String[] argu) {
+//		int i=0;
+//		while (i < 11){
+//			System.out.println(i);
+//			i++;
+//			}
+//	}
+//}
+//WAP to print the number from 1 to 10 using while loop
+//public class Openchrome{
+//	public static void main(String[] argu) {
+//		int i= 0;
+//		while(i<11) {
+//			System.out.println(i);
+//			i++;
+//		}
+//	}
+//}
+//WAP to print the number from 1 -10 using do while loop
+
+//public class Openchrome{
+//	public static void main(String[] argu) {
+//		int i = 0;
+//		do {
+//			System.out.println(i);
+//			i++;
+//		}
+//		while(i< 11);
+//	}
+//}
+/*public class Openchrome{
+	public static void main(String[] argu) {
+		int i = 0;
+		do {
+			System.out.println(i);
+			i++;
+		}
+		while(i< 11);
+	}
+}*/
+
+/*public class Openchrome{
+	public static void main(String[] argu) {
+		int i = 0;
+		do {
+			System.out.println(i);
+			i++;
+		}
+		while (i < 10);
+	}
+}*/
+
+/*public class Openchrome{
+	public static void main(String[] argu) {
+		for (int i=0; i<18; i++ ) {
+			System.out.println(i);
+		}
+	}
+}*/
+
+/*public class Openchrome{
+	public static void main(String[] argu) {
+		for (int a = 0; a <= 18; a++) {
+			System.out.println(a);
+		}
+	}
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*public class Openchrome{
+	public static void main(String[] argu){
+		int j=2;
+		for(int i = 1; i<=10; i++) {
+			System.out.println(i*j);
+		}
+	}
+}*/
+//
+//*
+//* *
+//* * *
+//* * * *
+//* * * * *
+
+/*public class Openchrome{
+	public static void main(String[] argu){
+		int row ;
+	for ( row=1; row<=5; row++){ // row
+		for(int col = 1; col<=row; col++) { // col
+			System.out.print("*");
+		}
+		System.out.println();
+	}	
+	}
+}*/
+
+/*public class Openchrome{
+	public static void main(String[] argu) {
+		int row;
+		for(row=1; row <= 5; row++) {
+			for(int col = 1; col<=row; col++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}*/
+
+/*public class Openchrome{
+	public static void main(String[] argu) {
+		int row;
+		for(row =1; row<=5; row++) {
+			for(int col = 1; col<=row; col++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}*/
+
+public class Openchrome{
+	public static void main(String[] argu) {
+		int a [] = new int [5];
+		
+		int a[0]=100;
+		int a[1]=200;
+		
+		System.out.println(a);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
