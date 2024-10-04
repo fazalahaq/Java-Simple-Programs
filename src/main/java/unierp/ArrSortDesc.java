e64e228d45fcc3a39916d8890d8785f943433786
unicode <unicode@USPL-329>
2024-10-04 09:38:46 +0530

    updated

--- src/main/java/unierp/ArrSortDesc.java
+++ src/main/java/unierp/ArrSortDesc.java
@@ -1,15 +1,34 @@
 package unierp;
 
-public class ArrSortDesc {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int arr[] = {1,2,3,5,7,8,9};
-		for (int i = 0; i<arr.length; i++) {
-			for (int j= i+1;j<arr.length; j++ ) {
-				if(a[i]<a[j])
-			}
-		}
-	}
-
-}
+public class ArrSortDesc {  
+	    public static void main(String[] args) {
+	        //Initialize array   
+	        int [] arr = {5, 2, 8, 7, 1};   
+	        int temp = 0;  
+	          
+	        //Displaying elements of original array  
+	        System.out.println("Elements of original array: ");  
+	        for (int i = 0; i < arr.length; i++) {   
+	            System.out.print(arr[i] + " ");  
+	        }  
+	          
+	        //Sort the array in descending order  
+	        for (int i = 0; i < arr.length; i++) {   
+	            for (int j = i+1; j < arr.length; j++) {   
+	               if(arr[i] < arr[j]){  
+	                   temp = arr[i];  
+	                   arr[i] = arr[j];  
+	                   arr[j] = temp;
+	               }   
+	            }   
+	        }  
+	          
+	        System.out.println();  
+	          
+	        //Displaying elements of array after sorting  
+	        System.out.println("Elements of array sorted in descending order: ");  
+	        for (int i = 0; i < arr.length; i++) {   
+	            System.out.print(arr[i] + " ");  
+	        }  
+	    }  
+	}  
